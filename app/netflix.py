5ee21f05577c0515d3208e94323701216ee04af6
Jucepos <jpostigo89@gmail.com>
2022-11-06 10:55:37 -0300

    prueba2

---
+++ app/netflix.py
@@ -0,0 +1,72 @@
+#Importamos librerias para limpieza
+
+import numpy as np
+import pandas as pd
+
+#Leemos el archivo con pandas
+
+df_netflix = pd.read_csv(r'C:\Users\jpost\Documents\proyecto1_sup07\csv\netflix.csv')
+
+#Hacemos una copia del archivo por cualquier cosa
+
+df_netflix_mod = df_netflix.copy()
+
+#Vemos las primeras cinco filas del dataframe
+
+df_netflix_mod.head()
+
+#Observamos que cantidad y tipos de datos tiene cada columna
+
+df_netflix_mod.info()
+
+#Cambiamos el tipo de dato de la columna date_added
+
+from datetime import datetime
+df_netflix_mod['date_added'] = pd.to_datetime(df_netflix_mod['date_added'])
+
+#Volvemos a observar para ver si realmente se cambio el tipo de dato
+
+df_netflix_mod.info()
+
+#Ordenamos los campos en forma ascendente por fecha (del mas viejo al mas nuevo) 
+
+df_netflix_mod.sort_values(by=['date_added'], inplace= True)
+
+#Reemplazamos los campos que tengan 'Not Given' en la columna director
+
+df_netflix_mod.loc[df_netflix_mod['director'].str.contains('Not Given', case = False), 'director'] = 'None'
+
+#Creamos los dataframes con las mascaras solo de los años que queremos 
+
+netf_2019 = df_netflix_mod[df_netflix_mod["release_year"] == 2019]
+netf_2020 = df_netflix_mod[df_netflix_mod["release_year"] == 2020]
+netf_2021 = df_netflix_mod[df_netflix_mod["release_year"] == 2021]
+
+#Transformamos en diccionario los dataframes
+
+net_2019= netf_2019.reset_index().to_dict(orient="index")
+net_2020= netf_2020.reset_index().to_dict(orient="index")
+net_2021= netf_2021.reset_index().to_dict(orient="index")
+
+
+#Creamos la Api
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def index():
+    return {'Elegi el catalogo que quieras ver /2019 /2020 /2021'}
+
+@app.get('/2019')
+async def index():
+    return {'2019' : net_2019}
+
+@app.get('/2020')
+async def index():
+    return {'2019' : net_2020}
+
+@app.get('/2021')
+async def index():
+    return {'2021' : net_2021}
